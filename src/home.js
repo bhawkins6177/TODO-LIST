e0ad415888645dc2f8eeb0b1bc54662514a04240
Brandon Hawkins <bhawkins6177@gmail.com>
2021-08-24 09:44:57 -0400

    form pop up

--- src/home.js
+++ src/home.js
@@ -1,23 +1,51 @@
 
-const content = document.querySelector('#content');
-const display = document.createElement('div');
-display.setAttribute('id','display');
-display.textContent = 'TEST';
-
-content.appendChild(display);
 
 
+    function homePage(){
+        const content = document.querySelector('#content');
+        const sideBar = document.createElement('div');
+        const display = document.createElement('div');
+        const taskDisplay = document.createElement('div');
+        const titleDisplay = document.createElement('div');
+        const form = document.createElement('form');
 
 
+        display.setAttribute('id','display');
+        sideBar.setAttribute('id','sideBar');
+        taskDisplay.setAttribute('id','taskDisplay');
+        titleDisplay.setAttribute('id','titleDisplay');
+        form.setAttribute('id','form');
+
+        
+        content.appendChild(sideBar);
+        content.appendChild(display);
+        display.appendChild(titleDisplay);
+        display.appendChild(taskDisplay);
+        taskDisplay.appendChild(form);
+        
+
+        taskDisplay.textContent = 'TEST';
 
 
+        
+        
+        }
+        export {homePage}
+        
+        
+        
+        
+        
+        
+        
+        
+        class tasks {
+            constructor(title, date, priority, description){
+                this.title = title,
+                this.date = date,
+                this.priority = priority,
+                this.description = description
+            }
+        }
 
 
-class tasks {
-    constructor(title, date, priority, description){
-        this.title = title,
-        this.date = date,
-        this.priority = priority,
-        this.description = description
-    }
-}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('it works! again?')
+
+   import { homePage } from "./home";
+   import './style.css';
+
+   homePage();
+
